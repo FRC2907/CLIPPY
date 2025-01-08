46ffaeee39249fde1ee5363257915e9987d81e6f
juh <josephreed2600@gmail.com>
2025-01-08 01:35:44 -0700

    remove built protos

--- grpc/built-protos/py/DoNothing_pb2.pyi
+++
@@ -1,6 +0,0 @@
-import Nada_pb2 as _Nada_pb2
-from google.protobuf import empty_pb2 as _empty_pb2
-from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
-
-DESCRIPTOR: _descriptor.FileDescriptor
--- grpc/built-protos/py/SystemTuner_pb2.pyi
+++
@@ -1,6 +0,0 @@
-from google.protobuf import empty_pb2 as _empty_pb2
-import SystemState_pb2 as _SystemState_pb2
-from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
-
-DESCRIPTOR: _descriptor.FileDescriptor
