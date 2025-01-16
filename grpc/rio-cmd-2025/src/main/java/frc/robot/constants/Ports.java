a8990950cfbb635746741feff7d52f1834b052f3
juh <josephreed2600@gmail.com>
2025-01-16 16:31:50 -0700

    portssssf

--- grpc/rio-cmd-2025/src/main/java/frc/robot/constants/Ports.java
+++ grpc/rio-cmd-2025/src/main/java/frc/robot/constants/Ports.java
@@ -4,9 +4,9 @@ public class Ports {
     public static final int DRIVER_CONTROLLER_PORT = 0;
 
     public static class CAN {
-        public static final int DT_FRONT_LEFT = 1;
-        public static final int DT_REAR_LEFT = 2;
-        public static final int DT_FRONT_RIGHT = 15;
-        public static final int DT_REAR_RIGHT = 16;
+        public static final int DT_FRONT_LEFT = 16;
+        public static final int DT_REAR_LEFT = 13;
+        public static final int DT_FRONT_RIGHT = 4;
+        public static final int DT_REAR_RIGHT = 1;
     }
 }
