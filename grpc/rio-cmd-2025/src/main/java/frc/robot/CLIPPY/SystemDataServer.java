4f1c68c6e68a93f742b7185e6c12d4f8117c8e9b
juh <josephreed2600@gmail.com>
2025-01-16 04:34:26 -0700

    fix missing system IDs in proto response and handle client cancelling connection

--- grpc/rio-cmd-2025/src/main/java/frc/robot/CLIPPY/SystemDataServer.java
+++ grpc/rio-cmd-2025/src/main/java/frc/robot/CLIPPY/SystemDataServer.java
@@ -1,5 +1,12 @@
 package frc.robot.CLIPPY;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.protobuf.Empty;
+
 import CLIPPY.control.SystemDataServerGrpc.SystemDataServerImplBase;
 import CLIPPY.control.SystemStateOuterClass.SystemIDs;
 import CLIPPY.control.SystemStateOuterClass.SystemState;
@@ -7,14 +14,8 @@ import CLIPPY.control.SystemStateOuterClass.SystemTags;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.flow.Registry;
+import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.google.protobuf.Empty;
 
 public class SystemDataServer extends SystemDataServerImplBase implements Subsystem {
 
@@ -36,14 +37,16 @@ public class SystemDataServer extends SystemDataServerImplBase implements Subsys
      */
     @Override
     public void getSystemIDs(SystemTags request, StreamObserver<SystemIDs> responseObserver) {
-        SystemIDs out = SystemIDs.newBuilder().buildPartial();
+        System.out.println("[info] Fetching system IDs for tags: " + request.toString());
+        SystemIDs out = SystemIDs.newBuilder().build();
         for (ITunableSystem sys : Registry.getInstance().systems.values()) {
-            if (request.getSystemTagsCount() == 0) out.toBuilder().addSystemIds(sys.getID());
+            if (request.getSystemTagsCount() == 0) out = out.toBuilder().addSystemIds(sys.getID()).build();
             else {
                 for (String tag : request.getSystemTagsList())
                 if (sys.hasTag(tag)) out.toBuilder().addSystemIds(sys.getID());
             }
         }
+        System.out.println("[info] Sending response: " + out);
         responseObserver.onNext(out.toBuilder().build());
         responseObserver.onCompleted();
     }
@@ -60,6 +63,11 @@ public class SystemDataServer extends SystemDataServerImplBase implements Subsys
      */
     @Override
     public void getSystemData(SystemIDs request, StreamObserver<SystemState> responseObserver) {
+        // https://github.com/grpc/grpc-java/blob/master/examples/src/main/java/io/grpc/examples/cancellation/CancellationServer.java#L99
+        ((ServerCallStreamObserver<SystemState>)responseObserver).setOnCancelHandler(() -> {
+            for (String id : clients.keySet())
+            clients.get(id).remove(responseObserver);
+        });;
         for (String id : request.getSystemIdsList())
         if (clients.containsKey(id)) clients.get(id).add(responseObserver);
         else clients.put(id, new ArrayList<>(List.of(responseObserver)));
